f09d167431f265866ace9cf10536963cdb5ffaf5
Tomas Gandulfo <tomgandulfo@gmail.com>
2023-11-14 15:45:45 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/tgsauzportfolio-react/', // Cambia "nombre-repositorio" por tu nombre de repositorio en GitHub
+  build: {
+    outDir: 'dist', // Carpeta de salida para los archivos construidos
+    assetsDir: '', // Carpeta de activos (si está en la raíz)
+    // Más configuraciones según tus necesidades
+  },
+});
